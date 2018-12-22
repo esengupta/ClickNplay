c272fbdb12bf6db84fe1a314cb2a5e57a57c31d7
enakshi sengupta <enakshi.kundu@gmail.com>
2018-12-21 23:49:29 -0500

    ss

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import ClickNplay from './components/ClickNplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ClickNplay />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
